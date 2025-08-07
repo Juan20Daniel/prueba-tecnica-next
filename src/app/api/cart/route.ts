2bbe795c651766e7d46ed6bea1413677ef00adb2
Juan20Daniel <juandaniel312000@gmail.com>
2025-08-06 19:56:32 -0600

    Configuración de backend

---
+++ src/app/api/cart/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+import { cart } from '@/app/lib/memory';
+
+const productAdded = (idProduct:number) => {
+    return cart.find(id => id === idProduct);
+}
+
+export async function GET(request: Request) { 
+  return NextResponse.json({message: 'Productos en el carrito', data:cart});
+}
+
+export async function POST(request: Request) {
+    const {idProduct} = await request.json();
+
+    if(!(!isNaN(Number(idProduct))) || idProduct < 1) return NextResponse.json({message:'El id del producto, no es válido.'}, {status:500});
+    
+    if(productAdded(idProduct)) return NextResponse.json({message:`Producto: ${idProduct} agregado al carrito`}, {status:200});
+
+    cart.push(idProduct);
+
+    return NextResponse.json({message:`Producto: ${idProduct} agregado al carrito`}, {status:200});
+}
---
+++ src/app/api/products/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+import path from 'path';
+import fs from 'fs';
+
+export async function GET(request: Request) {
+    const filePath = path.join(process.cwd(), 'data.json');
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const json = JSON.parse(fileContent);
+    
+    return NextResponse.json({message:'obtener todos los productos', data:json}, {status:200});
+}
